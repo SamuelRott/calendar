abde9376c4a9db03932c3b81af7b62c78d9e222c
samuelrott <rott.samuel@gmail.com>
2019-07-07 22:02:00 +0200

    add scss paths to tests

--- karma.conf.js
+++ karma.conf.js
@@ -20,6 +20,9 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    stylePreprocessorOptions: {
+      "includePaths": ["src/scss"]
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
