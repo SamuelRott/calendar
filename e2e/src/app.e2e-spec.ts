e63f87cb598925ca1cd9b11f231c7335434de911
samuelrott <rott.samuel@gmail.com>
2019-07-08 13:09:52 +0200

    typo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-Calendar!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-calendar!');
   });
 
   afterEach(async () => {
